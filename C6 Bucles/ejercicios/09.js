fc52715c35068589939a9f9faf9c53eb07669f39
Jesusspeed <jesusspeed_005@hotmail.com>
2024-11-08 21:45:48 -0500

    New finish

--- C6 Bucles/ejercicios/09.js
+++ C6 Bucles/ejercicios/09.js
@@ -5,6 +5,15 @@ function conection(status) {
   // De lo contrario, presumimos que el usuario está "Offline".
   // Retornar el estado de conexión del usuario.
   // Tu código:
-}
+  if(status === 1){
+  status = "Online"
+  }else if(status === 2){
+    status = "Away"
+  }else  if(status != 1 || status != 2){
+    status = "Offline"
+  }
+  return status
+};
+console.log(conection(2))
 
 module.exports = conection;
--- C6 Bucles/ejercicios/15.js
+++ C6 Bucles/ejercicios/15.js
@@ -6,6 +6,9 @@ function obtenerDiaSemana(numero) {
   // Si el número no corresponde a un día de la semana, retorna
   // el string "No es un dia de la semana"
   // Tu código:
-}
+    const dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+    return (numero >= 1 && numero <= 7) ? dias[numero -1]: "No es un dia de la semana"
+};
+console.log(obtenerDiaSemana(7))
 
 module.exports = obtenerDiaSemana;
